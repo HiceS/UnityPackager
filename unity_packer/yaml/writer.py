dc0f35529ab2f480f0167a12201cb4f0d0a4e309
Shawn Hice <shawn.hice@autodesk.com>
2020-11-15 22:59:29 -0800

    Worked the yaml generator to create the correct bindings

---
+++ unity_packer/yaml/writer.py
@@ -0,0 +1,29 @@
+"""
+Writer will be used to parse and form new yaml items from defined templates using delimeters.
+
+in this case the delimeters are ` $ item $ `
+
+This can use reflection to look at the object supplied in order to fill in the missing items in the string.
+
+"""
+
+import re
+from typing import List
+
+from mesh import meshyaml
+
+delim = r'\$(.*?)\$'
+
+def GenerateYamlIdentifiers() -> List[str]:
+    """ Generates named bindings for the yaml object
+
+    Returns:
+        List[str]: all of the bindings
+    """
+    iter = re.finditer(delim, meshyaml)
+    mesh_identifiers = []
+    for index in iter:
+        # print("MeshYaml - \t" + meshyaml[index.start(0): index.end(0)])
+        mesh_identifiers.append(meshyaml[index.start(0): index.end(0)])
+
+    return mesh_identifiers
